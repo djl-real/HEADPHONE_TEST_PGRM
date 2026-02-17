cd98a8f41b1a9eeefd41b1d1e7f01fd03c864dfe
unknown <djland410@gmail.com>
2026-02-17 15:26:01 -0500

    date update

--- run.bat
+++ run.bat
@@ -1,11 +1,13 @@
 @echo off
-REM Run the Python application
+REM Activate the virtual environment
+call venv\Scripts\activate.bat
 
+REM Run the Python application
 REM Optional: set the path to Python 3 explicitly if needed
 REM set PYTHON_PATH=C:\Python310\python.exe
 
 REM Run the main window script
-python3 .\main_window.py
+python .\main_window.py
 
 REM Pause to see any output/errors after the program exits
 pause
